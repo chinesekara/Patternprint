1613acb5e39c9e88f56a0e04ebecca483b559d34
chinesekara <1169668107boo@gmail.com>
2018-09-27 20:47:06 -0400

    Print square

--- src/patternprint/PatternPrint.java
+++ src/patternprint/PatternPrint.java
@@ -21,7 +21,7 @@ public class PatternPrint {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         for (int i=0;i<n;i++) {
-            for (int j=0;j<i+1;j++) {
+            for (int j=0;j<n;j++) {
                 System.out.print("*");
             }
             System.out.println();
