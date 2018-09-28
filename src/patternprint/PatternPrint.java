5ac1df1f00a4e1fe8fa335bbfd2fa59b6ac620b9
chinesekara <1169668107boo@gmail.com>
2018-09-27 20:54:25 -0400

    Print Diamond

--- src/patternprint/PatternPrint.java
+++ src/patternprint/PatternPrint.java
@@ -18,14 +18,29 @@ public class PatternPrint {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        for (int i=0;i<n;i++) {
-            for (int j=0;j<i+1;j++) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int space = n / 2;
+        int star = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int k = 0; k < space; k++) {
+                System.out.print(" ");
+
+            }
+            for (int j = 0; j < star; j++) {
                 System.out.print("*");
             }
             System.out.println();
+            if (i < n / 2) {
+                star += 2;
+                space -= 1;
+            } else {
+                star -= 2;
+                space += 1;
+
+            }
         }
     }
-    
+
 }
